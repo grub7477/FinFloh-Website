464ab029683f8b3c3b61ffaa508f8964b5af1db7
finfloh <gaurav.bairagi@finfloh.com>
2026-01-26 15:01:08 +0530

    done

--- utils/utmUrls.js
+++ utils/utmUrls.js
@@ -1,62 +1,68 @@
+// NOTE: UTM parameters should only be used for EXTERNAL links (email campaigns, social media, paid ads)
+// Internal navigation links should use clean URLs to avoid duplicate content issues in sitemaps
 const generateUtmUrls = (pageName) => ({
-  pricing_navbar: `https://finfloh.com/pricing?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  freeTrial_navbar: `https://finfloh.com/free-trial?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  bookDemo_navbar: `https://finfloh.com/book-a-demo?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  bookDemo_header: `https://finfloh.com/book-a-demo?utm_source=${pageName}_page_first&utm_medium=finfloh_website`,
-  bookDemo_newsLetter: `https://finfloh.com/book-a-demo?utm_source=${pageName}_page_last&utm_medium=finfloh_website`,
-  bookDemo_footer: `https://finfloh.com/book-a-demo?utm_source=${pageName}_page_footer&utm_medium=finfloh_website`,
-  logo_navbar: `https://finfloh.com/?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  logo_footer: `https://finfloh.com/?utm_source=${pageName}_page_footer&utm_medium=finfloh_website`,
-  about_us_navbar: `https://finfloh.com/about-us?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
+  // Internal navigation - using clean URLs (no UTM parameters)
+  pricing_navbar: `/pricing`,
+  freeTrial_navbar: `/free-trial`,
+  bookDemo_navbar: `/book-a-demo`,
+  bookDemo_header: `/book-a-demo`,
+  bookDemo_newsLetter: `/book-a-demo`,
+  bookDemo_footer: `/book-a-demo`,
+  logo_navbar: `/`,
+  logo_footer: `/`,
+  about_us_navbar: `/about-us`,
 
-  accountsReceivableCollections_navbar: `https://finfloh.com/accounts-receivable-collections?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  accountsReceivableCollections_otherProducts: `https://finfloh.com/accounts-receivable-collections?utm_source=${pageName}_page_other_products&utm_medium=finfloh_website`,
-  accountsReceivableCollections_footer: `https://finfloh.com/accounts-receivable-collections?utm_source=${pageName}_page_footer&utm_medium=finfloh_website`,
+  accountsReceivableCollections_navbar: `/accounts-receivable-collections`,
+  accountsReceivableCollections_otherProducts: `/accounts-receivable-collections`,
+  accountsReceivableCollections_footer: `/accounts-receivable-collections`,
 
-  creditdecisioning_navbar: `https://finfloh.com/credit-decisioning?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  creditdecisioning_otherProducts: `https://finfloh.com/credit-decisioning?utm_source=${pageName}_page_other_products&utm_medium=finfloh_website`,
-  creditdecisioning_footer: `https://finfloh.com/credit-decisioning?utm_source=${pageName}_page_footer&utm_medium=finfloh_website`,
+  creditdecisioning_navbar: `/credit-decisioning`,
+  creditdecisioning_otherProducts: `/credit-decisioning`,
+  creditdecisioning_footer: `/credit-decisioning`,
 
-  automatedCashApplication_navbar: `https://finfloh.com/automated-cash-application-software?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  automatedCashApplication_otherProducts: `https://finfloh.com/automated-cash-application-software?utm_source=${pageName}_page_other_products&utm_medium=finfloh_website`,
-  automatedCashApplication_footer: `https://finfloh.com/automated-cash-application-software?utm_source=${pageName}_page_footer&utm_medium=finfloh_website`,
+  automatedCashApplication_navbar: `/automated-cash-application-software`,
+  automatedCashApplication_otherProducts: `/automated-cash-application-software`,
+  automatedCashApplication_footer: `/automated-cash-application-software`,
 
-  integrationsAndInvoiceVerification_navbar: `https://finfloh.com/invoice-validation-automation?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  integrationsAndInvoiceVerification_otherProducts: `https://finfloh.com/invoice-validation-automation?utm_source=${pageName}_page_other_products&utm_medium=finfloh_website`,
-  integrationsAndInvoiceVerification_footer: `https://finfloh.com/invoice-validation-automation?utm_source=${pageName}_page_footer&utm_medium=finfloh_website`,
+  integrationsAndInvoiceVerification_navbar: `/invoice-validation-automation`,
+  integrationsAndInvoiceVerification_otherProducts: `/invoice-validation-automation`,
+  integrationsAndInvoiceVerification_footer: `/invoice-validation-automation`,
 
-  collaborativeCommunication_navbar: `https://finfloh.com/collaborative-communication?utm_source=${pageName}_page_navbar&utm_medium=finfloh_website`,
-  collaborativeCommunication_otherProducts: `https://finfloh.com/collaborative-communication?utm_source=${pageName}_page_other_products&utm_medium=finfloh_website`,
-  collaborativeCommunication_footer: `https://finfloh.com/collaborative-communication?utm_source=${pageName}_page_footer&utm_medium=finfloh_website`,
+  collaborativeCommunication_navbar: `/collaborative-communication`,
+  collaborativeCommunication_otherProducts: `/collaborative-communication`,
+  collaborativeCommunication_footer: `/collaborative-communication`,
 });
 
+// NOTE: UTM parameters should only be used for EXTERNAL links (email campaigns, social media, paid ads)
+// Internal navigation links should use clean URLs to avoid duplicate content issues in sitemaps
 export const generatehomeUtmUrls = (pageName) => ({
-  homePage_pricing_navbar: `https://finfloh.com/pricing?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_freeTrial_navbar: `https://finfloh.com/free-trial?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_bookDemo_navbar: `https://finfloh.com/book-a-demo?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_bookDemo_header: `https://finfloh.com/demo-request-successful?utm_source=${pageName}_first&utm_medium=finfloh_website`,
-  homePage_bookDemo_newsLetter: `https://finfloh.com/demo-request-successful?utm_source=${pageName}_last&utm_medium=finfloh_website`,
-  homePage_bookDemo_footer: `https://finfloh.com/book-a-demo?utm_source=${pageName}_footer&utm_medium=finfloh_website`,
+  // Internal navigation - using clean URLs (no UTM parameters)
+  homePage_pricing_navbar: `/pricing`,
+  homePage_freeTrial_navbar: `/free-trial`,
+  homePage_bookDemo_navbar: `/book-a-demo`,
+  homePage_bookDemo_header: `/demo-request-successful`,
+  homePage_bookDemo_newsLetter: `/demo-request-successful`,
+  homePage_bookDemo_footer: `/book-a-demo`,
 
-  homePage_accountsReceivableCollections_navbar: `https://finfloh.com/accounts-receivable-collections?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_accountsReceivableCollections_central: `https://finfloh.com/accounts-receivable-collections?utm_source=${pageName}_central&utm_medium=finfloh_website`,
-  homePage_accountsReceivableCollections_footer: `https://finfloh.com/accounts-receivable-collections?utm_source=${pageName}_footer&utm_medium=finfloh_website`,
+  homePage_accountsReceivableCollections_navbar: `/accounts-receivable-collections`,
+  homePage_accountsReceivableCollections_central: `/accounts-receivable-collections`,
+  homePage_accountsReceivableCollections_footer: `/accounts-receivable-collections`,
 
-  homePage_creditdecisioning_navbar: `https://finfloh.com/credit-decisioning?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_creditdecisioning_central: `https://finfloh.com/credit-decisioning?utm_source=${pageName}_central&utm_medium=finfloh_website`,
-  homePage_creditdecisioning_footer: `https://finfloh.com/credit-decisioning?utm_source=${pageName}_footer&utm_medium=finfloh_website`,
+  homePage_creditdecisioning_navbar: `/credit-decisioning`,
+  homePage_creditdecisioning_central: `/credit-decisioning`,
+  homePage_creditdecisioning_footer: `/credit-decisioning`,
 
-  homePage_automatedCashApplication_navbar: `https://finfloh.com/automated-cash-application-software?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_automatedCashApplication_central: `https://finfloh.com/automated-cash-application-software?utm_source=${pageName}_central&utm_medium=finfloh_website`,
-  homePage_automatedCashApplication_footer: `https://finfloh.com/automated-cash-application-software?utm_source=${pageName}_footer&utm_medium=finfloh_website`,
+  homePage_automatedCashApplication_navbar: `/automated-cash-application-software`,
+  homePage_automatedCashApplication_central: `/automated-cash-application-software`,
+  homePage_automatedCashApplication_footer: `/automated-cash-application-software`,
 
-  homePage_integrationsAndInvoiceVerification_navbar: `https://finfloh.com/invoice-validation-automation?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_integrationsAndInvoiceVerification_central: `https://finfloh.com/invoice-validation-automation?utm_source=${pageName}_central&utm_medium=finfloh_website`,
-  homePage_integrationsAndInvoiceVerification_footer: `https://finfloh.com/invoice-validation-automation?utm_source=${pageName}_footer&utm_medium=finfloh_website`,
+  homePage_integrationsAndInvoiceVerification_navbar: `/invoice-validation-automation`,
+  homePage_integrationsAndInvoiceVerification_central: `/invoice-validation-automation`,
+  homePage_integrationsAndInvoiceVerification_footer: `/invoice-validation-automation`,
 
-  homePage_collaborativeCommunication_navbar: `https://finfloh.com/collaborative-communication?utm_source=${pageName}_navbar&utm_medium=finfloh_website`,
-  homePage_collaborativeCommunication_central: `https://finfloh.com/collaborative-communication?utm_source=${pageName}_central&utm_medium=finfloh_website`,
-  homePage_collaborativeCommunication_footer: `https://finfloh.com/collaborative-communication?utm_source=${pageName}_footer&utm_medium=finfloh_website`,
+  homePage_collaborativeCommunication_navbar: `/collaborative-communication`,
+  homePage_collaborativeCommunication_central: `/collaborative-communication`,
+  homePage_collaborativeCommunication_footer: `/collaborative-communication`,
 });
 
 export default generateUtmUrls;
