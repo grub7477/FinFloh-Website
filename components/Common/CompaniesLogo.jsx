be57ec2770b43c7ee1a8ff80318b85c77edaad1b
nithilthomas-finfloh <142003424+nithilthomas-finfloh@users.noreply.github.com>
2026-03-02 12:39:34 +0530

    finbox logo addition

--- components/Common/CompaniesLogo.jsx
+++ components/Common/CompaniesLogo.jsx
@@ -23,23 +23,43 @@ const CompaniesLogo = () => {
             <div className="logos-slide">
               <img alt="gupshup-icon" src="/images/company-logos/Gupshup.svg" />
               <img alt="yellow_ai" src="/images/company-logos/yellow_ai.svg" />
-              <img alt="resustanability" src="/images/company-logos/Logo-red-1.svg" />
+              <img
+                alt="resustanability"
+                src="/images/company-logos/Logo-red-1.svg"
+              />
               <img alt="cactus" src="/images/company-logos/Cactus.svg" />
-              <img alt="knowlarity" src="/images/company-logos/knowlarity.svg" />
+              <img
+                alt="knowlarity"
+                src="/images/company-logos/knowlarity.svg"
+              />
               <img alt="Kaleyra" src="/images/company-logos/Kaleyra.svg" />
-              <img alt="Pulsepro_new" src="/images/company-logos/Pulsepro_new.svg" />
+              <img
+                alt="Pulsepro_new"
+                src="/images/company-logos/Pulsepro_new.svg"
+              />
               <img alt="Waylit" src="/images/company-logos/waylift.svg" />
               <img alt="bizom-icon" src="/images/company-logos/bizom.svg" />
+              <img alt="finbox" src="/images/company-logos/finbox.svg" />
 
               <img alt="gupshup-icon" src="/images/company-logos/Gupshup.svg" />
               <img alt="yellow_ai" src="/images/company-logos/yellow_ai.svg" />
-              <img alt="resustanability" src="/images/company-logos/Logo-red-1.svg" />
+              <img
+                alt="resustanability"
+                src="/images/company-logos/Logo-red-1.svg"
+              />
               <img alt="cactus" src="/images/company-logos/Cactus.svg" />
-              <img alt="knowlarity" src="/images/company-logos/knowlarity.svg" />
+              <img
+                alt="knowlarity"
+                src="/images/company-logos/knowlarity.svg"
+              />
               <img alt="Kaleyra" src="/images/company-logos/Kaleyra.svg" />
-              <img alt="Pulsepro_new" src="/images/company-logos/Pulsepro_new.svg" />
+              <img
+                alt="Pulsepro_new"
+                src="/images/company-logos/Pulsepro_new.svg"
+              />
               <img alt="Waylit" src="/images/company-logos/waylift.svg" />
               <img alt="bizom-icon" src="/images/company-logos/bizom.svg" />
+              <img alt="finbox" src="/images/company-logos/finbox.svg" />
             </div>
           </div>
         </div>
